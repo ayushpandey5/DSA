ec7f7f6f2e0052289592527522ea59be0854c955
Ayush Pandey <ayushpandey5@icloud.com>
2024-05-16 15:25:22 +1000

    print string n time recursively

--- 2 - Basic Recursion/recursion.py
+++ 2 - Basic Recursion/recursion.py
@@ -3,5 +3,11 @@ def printToN(i,n):
         return
     print(i)
     printToN(i+1,n)
-
 printToN(1,10)
+
+def printStringNTimes(count,name,n):
+    if count > n:
+        return
+    print(name)
+    printStringNTimes(count+1,name,n)
+printStringNTimes(1,"Hello",5)
