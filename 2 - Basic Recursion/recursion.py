641d73dd03de6e6d3b00f52929fb169846aae647
Ayush Pandey <ayushpandey5@icloud.com>
2024-05-16 15:29:59 +1000

    print n to 1 recursively

--- 2 - Basic Recursion/recursion.py
+++ 2 - Basic Recursion/recursion.py
@@ -3,11 +3,18 @@ def printToN(i,n):
         return
     print(i)
     printToN(i+1,n)
-printToN(1,10)
+#printToN(1,10)
 
 def printStringNTimes(count,name,n):
     if count > n:
         return
     print(name)
     printStringNTimes(count+1,name,n)
-printStringNTimes(1,"Hello",5)
+#printStringNTimes(1,"Hello",5)
+
+def printNTo1(i,num):
+    if i < 1:
+        return
+    print(i)
+    printNTo1(i-1,num)
+#printNTo1(10,10)
