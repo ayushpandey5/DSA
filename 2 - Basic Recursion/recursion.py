650139ba3ddea931b5e33f4e825b0cd45c4e38d0
Ayush Pandey <ayushpandey5@icloud.com>
2024-05-16 19:47:49 +1000

    functional recursion  sum of series

--- 2 - Basic Recursion/recursion.py
+++ 2 - Basic Recursion/recursion.py
@@ -19,9 +19,18 @@ def printNTo1(i,num):
     printNTo1(i-1,num)
 #printNTo1(10,10)
 
+# Parameterized
 def sumOfSeries(i,n,total=0):
     if i > n:
         print(total)
         return
     sumOfSeries(i+1,n,total+i)
 #sumOfSeries(1,3)
+
+# Function-based
+def sumOfSeries1(n,):
+    if n == 0:
+        return 0
+    return n + sumOfSeries1(n-1)
+
+print(sumOfSeries1(3))
