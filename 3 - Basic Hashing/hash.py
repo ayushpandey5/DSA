f2c6dc03b7d4c180ae71cc2e995c2eb43015f2e1
Ayush Pandey <ayushpandey5@icloud.com>
2024-05-19 15:13:58 +1000

    0(1) find num of element

--- 3 - Basic Hashing/hash.py
+++ 3 - Basic Hashing/hash.py
@@ -11,7 +11,7 @@ def findNum(n,arr):
 def hashArray(arr):
     hash_array=[0] * (max(arr) + 1) #max instead of len to ensure IndexError doesnot occur if any value of arr is more than len(arr)
     for i in range(len(arr)):
-        hash_array[arr[i]] += 1
-    return hash_array
+        hash_array[arr[i]] += 1 
+    return hash_array,hash_array[1]
 
 print(hashArray([1,2,3,4,5,10,12,1]))
