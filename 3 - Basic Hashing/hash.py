5133975bfed9300850243b22d80884d78b3a3cbf
Ayush Pandey <ayushpandey5@icloud.com>
2024-05-19 17:53:21 +1000

    print both hash array with element and number of occurances.

--- 3 - Basic Hashing/hash.py
+++ 3 - Basic Hashing/hash.py
@@ -11,8 +11,10 @@ def findNum(n,arr):
 def hashArray(arr):
     hash_array=[0] * (max(arr) + 1) #max instead of len to ensure IndexError doesnot occur if any value of arr is more than len(arr)
     for i in range(len(arr)):
-        hash_array[arr[i]] += 1 
-    return hash_array,hash_array[1]
+        hash_array[arr[i]] += 1
+
+    for i in range(len(hash_array)):
+        print(f"{i} -> {hash_array[i]}")
 #print(hashArray([1,2,3,4,5,10,12,1]))
 
 
@@ -20,5 +22,6 @@ def stringHash(s):
     string_hash = [0] * 26
     for i in range(len(s)):
         string_hash[ord(s[i]) - ord('a')] += 1
-    return string_hash,string_hash[ord('l') - ord('a')]
+    for i in range(len(string_hash)):
+        print(f"{chr((i+ord('a')))} -> {string_hash[i]}")
 print(stringHash("hello"))
