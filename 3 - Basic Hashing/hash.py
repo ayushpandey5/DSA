b45bee9bf1ade5342d8b5f4f20136db4050fdff2
Ayush Pandey <ayushpandey5@icloud.com>
2024-05-19 15:12:09 +1000

    hash array

---
+++ 3 - Basic Hashing/hash.py
@@ -0,0 +1,17 @@
+def findNum(n,arr):
+    count = 0
+    for i in range(len(arr)):
+        if n == arr[i]:
+            count += 1
+    return count
+#print(findNum(1,[1,2,3,4,5,1,2,1]))
+## Extremely high computation time
+
+## To solve this hashing is used.
+def hashArray(arr):
+    hash_array=[0] * (max(arr) + 1) #max instead of len to ensure IndexError doesnot occur if any value of arr is more than len(arr)
+    for i in range(len(arr)):
+        hash_array[arr[i]] += 1
+    return hash_array
+
+print(hashArray([1,2,3,4,5,10,12,1]))
